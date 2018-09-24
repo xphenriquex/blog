facd317c76cc2f380da39fd658b535cab7eb2151
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-24 11:08:54 -0300

    Adc conteudo topo

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
+Vue.component('topo', require('./components/Topo.vue'));
 
 const app = new Vue({
     el: '#app'
