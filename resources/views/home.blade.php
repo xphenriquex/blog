32914a9e2513c4c388371c511986024a6ea7a841
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-25 17:49:06 -0300

    Adc paineis ao home .blade e alterando o titulo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <examplo></examplo>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Dashboard">
+                    Teste de conteúdo..
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 1">
+                                Teste de conteúdo..
+                            </painel>        
+                        </div>
+
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 2">
+                                Teste de conteúdo..
+                            </painel>        
+                        </div>
+
+                        <div class="col-md-4">
+                            <painel titulo="Conteúdo 3">
+                                Teste de conteúdo..
+                            </painel>        
+                        </div>
+                    </div>
+
+
+
+                </painel>
+            </div>
+        </div>
+    </div>
+
 @endsection
