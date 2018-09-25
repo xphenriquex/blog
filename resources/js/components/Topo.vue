56e80016b66f61101ad276aef73a321138b5042f
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-25 13:41:28 -0300

    Adc tag slot

--- resources/js/components/Topo.vue
+++ resources/js/components/Topo.vue
@@ -15,6 +15,7 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <slot></slot>
                     </ul>
                 </div>
             </div>
