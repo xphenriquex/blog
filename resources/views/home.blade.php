8caed49fc329250f04346b458c81c7cddfd7273f
Paulo Henrique <p.henriquerm@hotmail.com>
2018-09-27 18:04:06 -0300

    uitlizando componente pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10 col-md-offset-1">
-                <painel titulo="Dashboard">
-                    Teste de conteúdo..
+    <pagina tamanho="4">
+        
+        <painel titulo="Dashboard">
+            Teste de conteúdo..
 
-                    <div class="row">
-                        <div class="col-md-4">
-                            <caixa qtd="80" titulo="Artigo" url="#" cor="orange" icone="ion ion-pie-graph" ></caixa>      
-                        </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <caixa qtd="80" titulo="Artigo" url="#" cor="orange" icone="ion ion-pie-graph" ></caixa>      
+                </div>
 
-                        <div class="col-md-4">
-                            <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker" ></caixa>      
-                        </div>
+                <div class="col-md-4">
+                    <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker" ></caixa>      
+                </div>
 
-                        <div class="col-md-4">
-                            <caixa qtd="3" titulo="Autores" url="#" cor="green" icone="ion ion-person" ></caixa>      
-                        </div>
-                    </div>
-                    
-                </painel>
+                <div class="col-md-4">
+                    <caixa qtd="3" titulo="Autores" url="#" cor="green" icone="ion ion-person" ></caixa>      
+                </div>
             </div>
-        </div>
-    </div>
+        </painel>
 
+    </pagina>
 @endsection
