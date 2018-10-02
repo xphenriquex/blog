26c52f28be6244cadca844322a97a2586f707e08
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-02 18:00:33 -0300

    Adcionando rota artigos

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    
+});
+
+Route::get('/artigos', 'ArtigoController')->name('artigos');
