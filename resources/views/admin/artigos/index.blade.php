8911b5498e8cdb7af891e0c8e97ae277cd71b7a1
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-04 16:28:24 -0300

    Passando links de ações

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -6,7 +6,8 @@
             <tabela-lista 
                 v-bind:titulos="['#', 'Título', 'Descrição']"
                 v-bind:itens="[['PHP OO', 'Curso de PHP OO'], ['VUE JS', 'Curso de VUE JS']]"    
-                
+                criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="87869876876"
+
             ></tabela-lista>
         </painel>
 
