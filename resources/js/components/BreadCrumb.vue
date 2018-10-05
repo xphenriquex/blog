c25664e9c9496ee4bf1c263bf2e056fa62a57ed4
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-05 17:52:55 -0300

    Criando templete breadCrumb

---
+++ resources/js/components/BreadCrumb.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Library</li>
+        </ol>
+    </nav>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
