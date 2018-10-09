6022817f8df81b2811e07a205be718021644ee9a
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-09 14:32:45 -0300

    Criando componente modal-link

--- resources/js/components/modal/ModalLink.vue
+++ resources/js/components/modal/ModalLink.vue
@@ -1,11 +1,41 @@
 <template>
-    
+    <div>
+
+        <button 
+            v-if="!tipo || (tipo != 'button' && tipo != 'link')" 
+            type="button" 
+            v-bind:class="css || 'btn btn-primary'" 
+            data-toggle="modal" 
+            v-bind:data-target="'#' + nome"
+            >{{ conteudo }}</button>
+
+        <button 
+            v-if="tipo == 'button'" 
+            type="button" 
+            v-bind:class="css || 'btn btn-primary'" 
+            data-toggle="modal" 
+            v-bind:data-target="'#' + nome"
+            >{{ conteudo }}</button>
+        
+        <a 
+            v-if="tipo == 'link'" 
+            href="#" 
+            v-bind:class="css || ''" 
+            data-toggle="modal" 
+            v-bind:data-target="'#' + nome"
+            >{{ conteudo }}</a>
+        
+    </div>
 </template>
 
 <script>
     export default {
+        props:['nome', 'tipo', 'conteudo', 'css'],
         mounted() {
-            console.log('Component mounted.')
-        }
+            // console.log("nome = " + this.nome);
+            // console.log("Tipo = " + this.tipo);
+            // console.log("conteudo = " + this.conteudo);
+            // console.log("css = " + this.css);
+        },
     }
 </script>
