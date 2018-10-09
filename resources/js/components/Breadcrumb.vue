da519060af472fa147608a23ffc228ca73ee7149
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-09 09:03:10 -0300

    tornando classe breadcrumb dinamica

--- resources/js/components/Breadcrumb.vue
+++ resources/js/components/Breadcrumb.vue
@@ -1,19 +1,11 @@
 <template>
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li v-for="item in lista" v-bind:class="defineClass">
+            <li v-for="item in lista" 
+                v-bind:class="{'breadcrumb-item': item.url, 'breadcrumb-item active': !item.url}" >
                 <a v-if="item.url" v-bind:href="item.url">{{ item.titulo }}</a>
                 <span v-if="!item.url">{{ item.titulo }}</span>
             </li> 
-            <!-- li class="breadcrumb-item"><a href="#">Library</a></li> -->
-            <li class="breadcrumb-item active" aria-current="page">Data</li>
-
-            <!-- <li v-for="(item, index) in lista"  v-bind:class="defineClass" >
-                <a >{{ item.titulo }}
-                </a>
-                <span v-if="!item.url">{{ item.titulo }}</span>
-            </li>
-            <li class="breadcrumb-item active" aria-current="page">Data</li> -->
         </ol>
     </nav>
 </template>
@@ -21,28 +13,9 @@
 <script>
     export default {
         props:['lista'], 
-        data: function(){
-            return {
-                teste: this.url,
-            }
-        },
         mounted: function() {
             console.log(this.lista);
             console.log(this.lista.length);
-        },
-        computed: {
-            defineClass: function(){
-
-                // return {
-                // }
-                // console.log(this.titulo);
-                if(this.titulo){
-                    return "breadcrumb-item active";
-                }else{
-                    return "";
-                }
-                
-            }
         }
     }
 </script>
