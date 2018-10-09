53dda8366a07b135c0a1b54cfdd33a7fb7d3c57a
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-09 14:32:25 -0300

    Utilizando componente modal-link

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,6 +4,7 @@
     <pagina tamanho="12">
         <painel titulo="Lista de artigos">
             <breadcrumb v-bind:lista="{{ $listaPaginas }}"></breadcrumb>
+            <modal-link nome="MeuModalTeste" tipo="link" conteudo="Criar" css=""></modal-link>
             <tabela-lista 
                 v-bind:titulos="['#', 'Título', 'Descrição']"
                 v-bind:itens="[[1, 'PHP OO', 'Curso de PHP OO'], [2, 'VUE JS', 'Curso de VUE JS']]"    
@@ -12,7 +13,27 @@
 
             ></tabela-lista>
         </painel>
-
     </pagina>
+
+    <modal nome="MeuModalTeste">
+        <painel titulo="Adcionar">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </painel>
+    </modal>
 @endsection
 
