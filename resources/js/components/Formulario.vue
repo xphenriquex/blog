4148b7d43689855261e06d51b184a9f324aa79a2
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-09 17:59:49 -0300

    Criando componente Formulario

---
+++ resources/js/components/Formulario.vue
@@ -0,0 +1,36 @@
+<template>
+    <form v-bind:class="css" v-bind:method="defineMetodo" v-bind:action="action" v-bind:enctype="ectype">
+        <input v-if="alterMetodo" type="hidden" name="_method" v-bind:value="alterMetodo" />
+        <input v-if="token" type="hidden" name="_token" v-bind:value="token" />
+        <slot></slot>
+        
+    </form>
+</template>
+
+<script>
+    export default {
+        props:['method', 'action', 'ectype', 'css', 'token'],
+        data: function(){
+            return  {
+                alterMetodo: ""
+            }
+        },
+        computed: {
+            defineMetodo: function(){
+                if(this.method.toLowerCase() ==  'post' || this.method.toLowerCase() ==  'get'){
+                    return this.method.toLowerCase();
+                }
+
+                if(this.method.toLowerCase() == "put"){
+                    this.alterMetodo = "put";
+                }
+
+                if(this.method.toLowerCase() == "delete"){
+                    this.alterMetodo = "delete";
+                }
+
+                return "post";
+            }
+        }
+    }
+</script>
