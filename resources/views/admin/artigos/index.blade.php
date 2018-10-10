6965dabdaecf22b428963af09f6fbac5277ad7c5
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 16:15:03 -0300

    Criando slot para adc botões dinamicamente

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -14,43 +14,43 @@
         </painel>
     </pagina>
 
-    <modal nome="adicionar">
-        <painel titulo="Adcionar">
-            <formulario css="" method="put" action="#add" enctype="multipart/form-data" token="123218544">
-                    <div class="form-group">
-                        <label for="titulo">Título</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
-                    </div>
-                    <div class="form-group">
-                        <label for="descricao">Descrição</label>
-                        <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Adicionar</button>
-            </formulario>
-        </painel>
+    <modal nome="adicionar" titulo="Adcionar">
+        <formulario id="formAdcionar" css="" method="put" action="#add" enctype="multipart/form-data" token="123218544">
+            <div class="form-group">
+                <label for="titulo">Título</label>
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
+            </div>
+        </formulario>
+        <span slot="botoes">
+                <button form="formAdcionar" type="submit" class="btn btn-primary">Adicionar</button>
+        </span>
     </modal>
 
-    <modal nome="editar">
-        <painel titulo="Editar">
-            <formulario css="" method="put" action="#add" enctype="multipart/form-data" token="123218544">
-                    <div class="form-group">
-                        <label for="titulo">Título</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Digite um título">
-                    </div>
-                    <div class="form-group">
-                        <label for="descricao">Descrição</label>
-                        <input type="text" class="form-control" id="descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Digite uma descrição">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Atualizar</button>
-            </formulario>
-        </painel>
+    <modal nome="editar" titulo="Adcionar">
+        <formulario id="formEditar" css="" method="put" action="#edit" enctype="multipart/form-data" token="123218544">
+            <div class="form-group">
+                <label for="titulo">Título</label>
+                <input type="text" class="form-control" id="titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Digite um título">
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" class="form-control" id="descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Digite uma descrição">
+            </div>
+        </formulario>
+        <span slot="botoes">
+                <button form="formEditar" type="submit" class="btn btn-primary">Atualizar</button>
+        </span>
+        
     </modal>
-    <modal nome="detalhe">
-        <painel v-bind:titulo="$store.state.item.titulo">
-            <p>
-                @{{ $store.state.item.descricao }}
-            </p>
-        </painel>
+
+    <modal nome="detalhe" v-bind:titulo="$store.state.item.titulo">
+        <p>
+            @{{ $store.state.item.descricao }}
+        </p>
     </modal>
 @endsection
 
