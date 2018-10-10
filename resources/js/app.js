81b1f9ff0bc94703af51482fe4faeea9b4ecb156
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 11:44:24 -0300

    Alteração app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,11 +9,28 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+
+//Vuex
+const store = new Vuex.Store({
+    state:{
+        itens:{teste: "Opa funcionou"}
+    },
+    mutations:{
+        setItens(state, obj){
+            state.itens = obj;
+        }
+    }
+});
+
 
 Vue.component('topo', require('./components/Topo.vue'));
 Vue.component('painel', require('./components/Painel.vue'));
@@ -26,5 +43,6 @@ Vue.component('modal-link', require('./components/modal/ModalLink.vue'));
 Vue.component('formulario', require('./components/Formulario.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store
 });
