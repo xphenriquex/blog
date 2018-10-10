0b7d8f69d5d6a29ae921d5f31d0cc89976a6ac66
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 11:45:54 -0300

    Criando modal para editar e detalhes

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,19 +4,50 @@
     <pagina tamanho="12">
         <painel titulo="Lista de artigos">
             <breadcrumb v-bind:lista="{{ $listaPaginas }}"></breadcrumb>
-            <modal-link nome="MeuModalTeste" tipo="link" conteudo="Criar" css=""></modal-link>
             <tabela-lista 
                 v-bind:titulos="['#', 'Título', 'Descrição']"
                 v-bind:itens="{{ $listaArtigos }}"    
                 criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="87869876876"
                 ordem="asc" ordemColum="2"
-
+                modal="sim"
             ></tabela-lista>
         </painel>
     </pagina>
 
-    <modal nome="MeuModalTeste">
+    <modal nome="adicionar">
         <painel titulo="Adcionar">
+            <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
+                    <div class="form-group">
+                        <label for="titulo">Título</label>
+                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
+                    </div>
+                    <div class="form-group">
+                        <label for="descricao">Descrição</label>
+                        <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Adicionar</button>
+            </formulario>
+        </painel>
+    </modal>
+
+    <modal nome="editar">
+        <painel titulo="Editar">
+            <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
+                    <div class="form-group">
+                        <label for="titulo">Título</label>
+                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
+                    </div>
+                    <div class="form-group">
+                        <label for="descricao">Descrição</label>
+                        <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Atualizar</button>
+            </formulario>
+        </painel>
+    </modal>
+
+    <modal nome="detalhes">
+        <painel titulo="Detalhes">
             <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
                     <div class="form-group">
                         <label for="titulo">Título</label>
