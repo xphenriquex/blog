f3febd7faf23abf1760f8b154b6a7c563e9e8a0a
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 14:31:08 -0300

    Criando logica para preenchimento

--- resources/js/components/modal/ModalLink.vue
+++ resources/js/components/modal/ModalLink.vue
@@ -1,35 +1,81 @@
 <template>
     <span>
+        <span v-if="item">
+            <button
+                v-on:click="preencheFormulario()"
+                v-if="!tipo || (tipo != 'button' && tipo != 'link')" 
+                type="button" 
+                v-bind:class="css || 'btn btn-primary'" 
+                data-toggle="modal" 
+                v-bind:data-target="'#' + nome"
+                >
+                    {{ conteudo }}
+            </button>
 
-        <button 
-            v-if="!tipo || (tipo != 'button' && tipo != 'link')" 
-            type="button" 
-            v-bind:class="css || 'btn btn-primary'" 
-            data-toggle="modal" 
-            v-bind:data-target="'#' + nome"
-            >{{ conteudo }}</button>
+            <button 
+                v-on:click="preencheFormulario()"
+                v-if="!tipo || (tipo != 'button' && tipo != 'link')" 
+                type="button" 
+                v-bind:class="css || 'btn btn-primary'" 
+                data-toggle="modal" 
+                v-bind:data-target="'#' + nome"
+                >
+                    {{ conteudo }}
+            </button>
 
-        <button 
-            v-if="tipo == 'button'" 
-            type="button" 
-            v-bind:class="css || 'btn btn-primary'" 
-            data-toggle="modal" 
-            v-bind:data-target="'#' + nome"
-            >{{ conteudo }}</button>
+            <a 
+                v-on:click="preencheFormulario()"
+                v-if="tipo == 'link'" 
+                href="#" 
+                v-bind:class="css || ''" 
+                data-toggle="modal" 
+                v-bind:data-target="'#' + nome"
+                >
+                    {{ conteudo }}
+            </a>
+        </span>
+
+        <span v-if="!item">
+            <button 
+                v-if="!tipo || (tipo != 'button' && tipo != 'link')" 
+                type="button" 
+                v-bind:class="css || 'btn btn-primary'" 
+                data-toggle="modal" 
+                v-bind:data-target="'#' + nome"
+                >
+                    {{ conteudo }}
+            </button>
+
+            <button 
+                    v-if="tipo == 'button'" 
+                    type="button" 
+                    v-bind:class="css || 'btn btn-primary'" 
+                    data-toggle="modal" 
+                    v-bind:data-target="'#' + nome"
+                    >
+                        {{ conteudo }}
+            </button>
         
-        <a 
-            v-if="tipo == 'link'" 
-            href="#" 
-            v-bind:class="css || ''" 
-            data-toggle="modal" 
-            v-bind:data-target="'#' + nome"
-            >{{ conteudo }}</a>
-        
+            <a 
+                v-if="tipo == 'link'" 
+                href="#" 
+                v-bind:class="css || ''" 
+                data-toggle="modal" 
+                v-bind:data-target="'#' + nome"
+                >
+                    {{ conteudo }}
+            </a>
+        </span>
     </span>
 </template>
 
 <script>
     export default {
-        props:['nome', 'tipo', 'conteudo', 'css'],
+        props:['nome', 'tipo', 'conteudo', 'css', 'item'],
+        methods: {
+            preencheFormulario: function(){
+                this.$store.commit('setItem', this.item);
+            }
+        }
     }
 </script>
