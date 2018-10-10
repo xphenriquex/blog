36b69da63d8f5df291f0d121fb466bc0dc12e8d7
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 14:30:48 -0300

    Utilizando preechimento de modal editar e detalhe

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -16,7 +16,7 @@
 
     <modal nome="adicionar">
         <painel titulo="Adcionar">
-            <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
+            <formulario css="" method="put" action="#add" enctype="multipart/form-data" token="123218544">
                     <div class="form-group">
                         <label for="titulo">Título</label>
                         <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
@@ -32,33 +32,24 @@
 
     <modal nome="editar">
         <painel titulo="Editar">
-            <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
+            <formulario css="" method="put" action="#add" enctype="multipart/form-data" token="123218544">
                     <div class="form-group">
                         <label for="titulo">Título</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
+                        <input type="text" class="form-control" id="titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Digite um título">
                     </div>
                     <div class="form-group">
                         <label for="descricao">Descrição</label>
-                        <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
+                        <input type="text" class="form-control" id="descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Digite uma descrição">
                     </div>
                     <button type="submit" class="btn btn-primary">Atualizar</button>
             </formulario>
         </painel>
     </modal>
-
-    <modal nome="detalhes">
-        <painel titulo="Detalhes">
-            <formulario css="aa" method="put" action="#add" enctype="multipart/form-data" token="123218544">
-                    <div class="form-group">
-                        <label for="titulo">Título</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Digite um título">
-                    </div>
-                    <div class="form-group">
-                        <label for="descricao">Descrição</label>
-                        <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Digite uma descrição">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Adicionar</button>
-            </formulario>
+    <modal nome="detalhe">
+        <painel v-bind:titulo="$store.state.item.titulo">
+            <p>
+                @{{ $store.state.item.descricao }}
+            </p>
         </painel>
     </modal>
 @endsection
