6593e82584776e2190227c450624e3495892bf20
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-10 18:04:00 -0300

    Criando model artigo

---
+++ app/Artigo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Artigo extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'titulo', 'descricao', 'conteudo', 'data',
+    ];
+
+    protected $dates = ['deleted_at'];
+}
