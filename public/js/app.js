fea6821367d5d3241ee0538ed65e771cacfffad3
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-19 15:42:47 -0300

    Altera app.css e app.js

--- public/js/app.js
+++ public/js/app.js
@@ -48788,7 +48788,7 @@ var content = __webpack_require__(48);
 if(typeof content === 'string') content = [[module.i, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
-var update = __webpack_require__(13)("fe492066", content, false, {});
+var update = __webpack_require__(13)("43096fe0", content, false, {});
 // Hot Module Replacement
 if(false) {
  // When the styles change, update the <style> tags
@@ -48966,7 +48966,7 @@ var content = __webpack_require__(54);
 if(typeof content === 'string') content = [[module.i, content, '']];
 if(content.locals) module.exports = content.locals;
 // add the styles to the DOM
-var update = __webpack_require__(13)("54b0d2e4", content, false, {});
+var update = __webpack_require__(13)("a4edf71e", content, false, {});
 // Hot Module Replacement
 if(false) {
  // When the styles change, update the <style> tags
