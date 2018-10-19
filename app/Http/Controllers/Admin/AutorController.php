10b9a68bf3358a022935c01b5ddeb8d633a8c9bd
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-19 18:09:58 -0300

    Criando AutorController

---
+++ app/Http/Controllers/Admin/AutorController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use App\User;
+use Validator;
+
+
+class AutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listaPaginas = json_encode([
+            ["titulo" => "Home", "url" => route('home')],
+            ["titulo" => "Lista de Autores", "url" => ''],
+        ]);
+
+        $listaModelo = 
+            User::select('id', 'name','email')
+            ->where('autor','=','S')
+            ->paginate(5);
+
+        return view('admin.autores.index', compact('listaPaginas', 'listaModelo'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dados = $request->all();
+        $validacao = Validator::make($dados, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|',
+        ]);
+
+        if($validacao->fails()){
+            return redirect()
+                ->back()
+                ->withErrors($validacao)
+                ->withInput();
+        }
+
+        $dados['password'] = Hash::make($dados['password']);
+
+        User::create($dados);
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $dados = $request->all();
+
+        if(isset($dados['password']) && $dados['password'] != ""){
+            $validacao = Validator::make($dados, [
+                'name' => 'required|string|max:255',
+                'email' => [
+                    'required','string','email','max:255',
+                    Rule::unique('users')->ignore($id)
+                ],
+                'password' => 'required|string|min:6|',
+            ]);
+
+            $dados['password'] = Hash::make($dados['password']);
+        }else{
+
+            $validacao = Validator::make($dados, [
+                'name' => 'required|string|max:255',
+                'email' => [
+                    'required','string','email','max:255',
+                    Rule::unique('users')->ignore($id)
+                ],
+            ]);
+            unset($dados["password"]);
+        }
+
+        
+
+        if($validacao->fails()){
+            return redirect()
+                ->back()
+                ->withErrors($validacao)
+                ->withInput();
+        }
+
+        User::find($id)->update($dados);
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+        return redirect()->back();
+    }
+}
