7ab67241a0842977585479dbc0f315e0a7724099
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-26 15:47:54 -0300

    Adc cartão para testar layout

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -1,96 +1,43 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/admin') }}">Admin</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+@section('content')
+    <pagina tamanho="12">
+        <painel titulo="Artigos">
+            
+            <div class="card-deck">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://gaea.com.br/wp-content/uploads/2016/09/tudo-que-voce-precisa-saber-sobre-desenvolvimento-de-software.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Desevolvimento web</h5>
+                        <p class="card-text">Aprenda tudo que gostaria de saber sobre desenvolvimento web e muito mais.</p>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-primary">Ler mais</a>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://gaea.com.br/wp-content/uploads/2016/09/tudo-que-voce-precisa-saber-sobre-desenvolvimento-de-software.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Desevolvimento web</h5>
+                        <p class="card-text">Aprenda tudo que gostaria de saber sobre desenvolvimento web e muito mais.</p>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-primary">Ler mais</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://gaea.com.br/wp-content/uploads/2016/09/tudo-que-voce-precisa-saber-sobre-desenvolvimento-de-software.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Desevolvimento web</h5>
+                        <p class="card-text">Aprenda tudo que gostaria de saber sobre desenvolvimento web e muito mais.</p>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-primary">Ler mais</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </body>
-</html>
+        </painel>
+    </pagina>
+@endsection
