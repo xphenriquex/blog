1ec1208d12c03a5cb31bc855f77f0e6ed2acd1be
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-29 18:02:49 -0300

    Configurando permissão de visualização para admin e autor

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,21 +7,26 @@
             
             <breadcrumb v-bind:lista="{{ $listaPaginas }}"></breadcrumb>
             <div class="row">
-                <div class="col-md-4">
-                    <caixa qtd="{{ $qtdArtigos }}" titulo="Artigos" url="{{ route('artigos.index') }}" cor="orange" icone="ion ion-pie-graph" ></caixa>      
-                </div>
-
-                <div class="col-md-4">
-                    <caixa qtd="{{ $qtdUsuarios }}" titulo="Usuários" url="{{ route('usuarios.index') }}" cor="blue" icone="ion ion-person-stalker" ></caixa>      
-                </div>
-
-                <div class="col-md-4">
-                    <caixa qtd="{{ $qtdAutores }}" titulo="Autores" url="{{ route('autores.index') }}" cor="green" icone="ion ion-person" ></caixa>      
-                </div>
-
-                <div class="col-md-4">
-                    <caixa qtd="{{ $qtdAdmins }}" titulo="Administradores" url="{{ route('adm.index') }}" cor="red" icone="ion ion-person" ></caixa>      
-                </div>
+                @can('autor')
+                    <div class="col-md-4">
+                        <caixa qtd="{{ $qtdArtigos }}" titulo="Artigos" url="{{ route('artigos.index') }}" cor="orange" icone="ion ion-pie-graph" ></caixa>      
+                    </div>    
+                @endcan
+                
+                @can('eAdmin')
+                    <div class="col-md-4">
+                        <caixa qtd="{{ $qtdUsuarios }}" titulo="Usuários" url="{{ route('usuarios.index') }}" cor="blue" icone="ion ion-person-stalker" ></caixa>      
+                    </div>
+    
+                    <div class="col-md-4">
+                        <caixa qtd="{{ $qtdAutores }}" titulo="Autores" url="{{ route('autores.index') }}" cor="green" icone="ion ion-person" ></caixa>      
+                    </div>
+    
+                    <div class="col-md-4">
+                        <caixa qtd="{{ $qtdAdmins }}" titulo="Administradores" url="{{ route('adm.index') }}" cor="red" icone="ion ion-person" ></caixa>      
+                    </div>    
+                @endcan
+                
             </div>
         </painel>
 
