21c1efb7f584acf7bc4d74247c946379b237d460
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-29 11:20:36 -0300

    Criando layout da pagina de artigo

--- resources/views/artigo.blade.php
+++ resources/views/artigo.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
     <pagina tamanho="12">
-        <painel titulo="Artigos">
-            <h2>{{$artigo->titulo}}</h2>
-
+        <painel>
+            <h2 align="center">{{$artigo->titulo}}</h2>
+            <h5 align="center">{{ $artigo->descricao }}</h5>
+            <p>
+                {!!$artigo->conteudo!!}
+            </p>
+            <p align="center">
+                <small>Por: {{ $artigo->user->name }} - {{ date('d/m/Y', strtotime($artigo->data)) }}</small>
+            </p>
         </painel>
     </pagina>
 
