702c56ad2b5a083b7444accb96ba8ae80fec1e4b
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-28 21:55:45 -0300

    Criando view artigo

---
+++ resources/views/artigo.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho="12">
+        <painel titulo="Artigos">
+            <h2>{{$artigo->titulo}}</h2>
+
+        </painel>
+    </pagina>
+
+
+@endsection
