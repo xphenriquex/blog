7432ed08a406f86247c8ceecc49cf496e623d9c1
Paulo Henrique <p.henriquerm@hotmail.com>
2018-10-30 18:01:32 -0300

    Adc input de busca

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -3,7 +3,19 @@
 @section('content')
     <pagina tamanho="12">
         <painel titulo="Artigos">
-            
+            <nav class="navbar navbar-light bg-light">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" 
+                        type="search" 
+                        name="busca"
+                        placeholder="Buscar" 
+                        aria-label="Search"
+                        value="{{ isset($busca) ? $busca : '' }}">
+                    <button class="btn btn-outline-success my-2 my-sm-0" 
+                    type="submit">Buscar</button>
+                </form>
+            </nav>
+
             <div class="card-deck">
                 @foreach ($lista as $item)
                     <artigo-card
@@ -12,8 +24,7 @@
                         titulo="{{ str_limit($item->titulo, 30, "...") }}"
                         descricao="{{ str_limit($item->descricao, 40, "...") }}"
                         link="{{ route('artigo', [$item->id, str_slug($item->titulo)] )}}"
-                        sm="3"
-                        md="4"
+                        largura="18"
                         >
                     </artigo-card>
                 @endforeach
